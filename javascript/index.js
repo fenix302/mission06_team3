502c9d3d7baa27f5b2e28a3442d8ebeabdd7a426
flyepic4 <flyepic4@gmail.com>
2022-12-09 00:16:03 +0900

    main_update

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+   $('document').ready(function () {
+      var options = { videoId: 'FBtWixtj4NQ', mute: true };
+      $('header, #main_visual, section, #content, .con, footer, .in3').tubular(options);
+   });
+});
